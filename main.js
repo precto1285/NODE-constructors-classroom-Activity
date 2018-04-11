8317af0acab5c187e02d75bbe1fe02909543706a
Philippe Recto <precto1285@gmal.com>
2018-04-11 08:52:04 -0700

    added main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+//requiring our Class module exported from Class.js
+var Class = require("./Class.js");
+
+//creating and storing a new classroom object
+var firstClass = new Class(Ahmed, 3187);
+
+//calling the addStudent method on our firstClass object
+firstClass.addStudent("Jacob", "Coding", 3.87);
+
+console.log(firstClass);
